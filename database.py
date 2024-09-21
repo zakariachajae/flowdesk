b9f0a65258df50c6a40ffc7b1979a93fa5b198ba
zakariachajae <zakariachajae@gmail.com>
2024-09-21 20:35:17 +0100

    helloworld

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root@localhost/designer"
+DATABASE_URL = "mysql+mysqlconnector://uhqp1kcpzvbjn0ja:XolvcP5iWxynYQ5Jcauy@bmrl2p3a5ocsn27lo153-mysql.services.clever-cloud.com:3306/bmrl2p3a5ocsn27lo153"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
